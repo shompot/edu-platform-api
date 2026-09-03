03b5e6c5a1a32ff95b0ee2cbd3afd503a58a9220
Cholpon <cholpon94@gmail.com>
2026-09-03 18:35:31 +0200

    Remove db credentials from source code using .env

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:dbaccess1234@localhost/edu_platform"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
